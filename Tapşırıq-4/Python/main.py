1e032503a19ff9b15eb7fb80b6aedeab81ade0c0
YasinVeliyev <veliyev.yasin@gmail.com>
2023-02-06 20:43:31 +0400

    Tapşırıq

---
+++ Tapşırıq-4/Python/main.py
@@ -0,0 +1,30 @@
+import pandas as pd
+import numpy as np
+from datetime import datetime
+
+data = pd.read_excel(".././REKTDATABASE.xlsx")
+
+data = data[data['Project'].notna()]
+data["FundsRecovered"].fillna(0,inplace=True)
+data["Category"].fillna("Other",inplace=True)
+data["IsVerifiedSourceCode"].fillna(0,inplace=True)
+data["IsPublicedTeam"].fillna(0,inplace=True)
+data.iloc[:,0]=data.iloc[:,0].apply(lambda x:int(x))
+
+data=data.loc[(data["Date"].notna())&(data["FundLost"]>0)]
+
+data["Amount"]=data["FundLost"]-data["FundsRecovered"]-data["FundsReturned"]
+
+data_monetary=data.groupby("ID").agg(Project=pd.NamedAgg(column="Project",aggfunc=lambda project:project),
+                                     Monetary=pd.NamedAgg(column="Amount",aggfunc="sum")).reset_index().set_index("ID")
+
+data_frequency = data.groupby("ID").agg(Frequency=pd.NamedAgg(column="Project", aggfunc="count")).reset_index().set_index("ID")
+
+m=data["Date"].max()
+data_recency=data.groupby("ID").agg(Recency=pd.NamedAgg(column="Date",aggfunc="max")).reset_index().set_index("ID")
+data_recency["Recency"]=(m-data_recency["Recency"])
+data_recency["Recency"]=data_recency["Recency"].apply(lambda d:d.days)
+
+rfm_data=data_monetary.join(data_frequency,on="ID",how="outer")
+rfm_data=rfm_data.join(data_recency,on="ID",how="outer")
+print(rfm_data)
